c609c784f12bfd408038694b4c0b1bc36a0e05b0
Rushi4031 <rushirandive31@gmail.com>
2024-12-10 13:23:09 +0530

    Step 5, added admin router

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { createUserRoutes, userRouter } = require('./routes/user');
-const { createCourseRoutes, courseRouter } = require('./routes/course'); // Fixed 'require' and missing export
+const {  userRouter } = require('./routes/user');
+const {  courseRouter } = require('./routes/course'); // Fixed 'require' and missing export
+const {  adminRouter } = require('./routes/admin'); 
 const app = express();
 
 // Fixed parentheses to use round brackets instead of curly braces
 app.use("/api/v1/user", userRouter);
+app.use("/api/v1/admin", adminRouter);
 app.use("/api/v1/course", courseRouter);
 
 // Properly initializing the routes
